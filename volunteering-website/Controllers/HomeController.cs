44ae35efbcba0d0d8882b5daf3946bb849d565bf
Al-MardasDev <94618007+dark0crystal@users.noreply.github.com>
2024-10-25 21:06:40 +0400

    add controller

---
+++ volunteering-website/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace volunteering_website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
