f5638f85cbfd6159d58b8556366e612e322c33ee
Al-MardasDev <94618007+dark0crystal@users.noreply.github.com>
2024-10-26 06:49:17 +0400

    api fix

---
+++ volunteering-website/Controllers/PostsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace volunteering_website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // Sample Posts class
+        public class Post
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = new List<Post>
+            {
+                new Post
+                {
+                    Id = 1,
+                    Name = "mazen"
+                }
+            };
+
+            return Ok(posts); // Return the list of posts as JSON
+        }
+    }
+}
